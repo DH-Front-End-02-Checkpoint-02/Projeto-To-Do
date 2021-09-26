e5cb70594cca57a80e3b55816a75c87310791b2e
Fer96carvalho <Fer96carvalho@root>
2021-09-26 11:59:11 -0400

    Correções de validação Login

--- js/script-login.js
+++ js/script-login.js
@@ -20,7 +20,7 @@ document.getElementById("botaoo").addEventListener('click', function entrar() {
 
   let senha = document.getElementById('senha#')
   let senhaLabel = document.querySelector('#senhaLabel')
-
+  
   let listaUser = []
 
   let userValid = {
@@ -31,35 +31,40 @@ document.getElementById("botaoo").addEventListener('click', function entrar() {
 
   listaUser = JSON.parse(localStorage.getItem('listaUser'))
 
-  listaUser.forEach((item) => {
-    if (email.value == item.email && senha.value == item.senha) {
-
-      userValid = {
-        nome: item.nome,
-        email: item.email,
-        senha: item.senha
-      }
-
-    }
-  })
-
-  if (email.value == userValid.email && senha.value == userValid.senha) {
-    
-    let mathRandom = Math.random().toString(16).substr(2)
-    let token = mathRandom + mathRandom;
-
-    localStorage.setItem('token', token)
-    localStorage.setItem('userLogado', JSON.stringify(userValid))
-    window.location.href = 'http://127.0.0.1:5500/to-do-list.html'
-    document.getElementById('form').reset()
-
-  } else {
-    emailLabel.setAttribute('style', 'color: red')
-    senhaLabel.setAttribute('style', 'color: red')
-    senha.setAttribute('style', 'border-color: red')
-    email.setAttribute('style', 'border-color: red')
-    document.getElementById('form').reset()
+  if(email.value == null || email.value == "" || senha.value == null || senha.value == ""){
+    alert('Digite um email e senha!')
     email.focus()
-    alert('Usuario ou senha Incorretos')
+  }else {
+    listaUser.forEach((item) => {
+      if (email.value == item.email && senha.value == item.senha) {
+  
+        userValid = {
+          nome: item.nome,
+          email: item.email,
+          senha: item.senha
+        }
+  
+      }
+    })
+  
+    if (email.value == userValid.email && senha.value == userValid.senha) {
+      
+      let mathRandom = Math.random().toString(16).substr(2)
+      let token = mathRandom + mathRandom;
+  
+      localStorage.setItem('token', token)
+      localStorage.setItem('userLogado', JSON.stringify(userValid))
+      window.location.href = 'http://127.0.0.1:5500/to-do-list.html'
+      document.getElementById('form').reset()
+  
+    } else {
+      emailLabel.setAttribute('style', 'color: red')
+      senhaLabel.setAttribute('style', 'color: red')
+      senha.setAttribute('style', 'border-color: red')
+      email.setAttribute('style', 'border-color: red')
+      document.getElementById('form').reset()
+      email.focus()
+      alert('Usuario ou senha Incorretos')
+    }
   }
 })
